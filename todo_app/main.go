399415b9712e9db52c7027ef2923280b2a46b45a
SteveMCWin <stevp003@gmail.com>
2026-07-22 13:04:47 +0200

    1.2: added the project todo app and dockerized it

---
+++ todo_app/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Server started in port %s\n", port)
+	http.ListenAndServe(":"+port, nil)
+}
