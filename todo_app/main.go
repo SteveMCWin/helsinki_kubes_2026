bf4315e2bbbfe0c2990de0359aa0908c5504276a
SteveMCWin <stevp003@gmail.com>
2026-07-23 01:18:37 +0200

    1.13: added some functionality to todo app

--- todo_app/main.go
+++ todo_app/main.go
@@ -5,13 +5,26 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+const maxTodoLength = 140
+
+type Todo struct {
+	Text string `json:"text"`
+	Done bool   `json:"done"`
+}
+
+type newTodoRequest struct {
+	Text string `json:"text"`
+}
+
 func fetchImage(path string) error {
-	resp, err := http.Get("https://picsum.photos/1200")
+	resp, err := http.Get("https://picsum.photos/600")
 	if err != nil {
 		return err
 	}
@@ -45,9 +58,48 @@ func main() {
 		}
 	}()
 
+	var mu sync.Mutex
+	todos := []Todo{
+		{Text: "Learn Kubernetes", Done: false},
+		{Text: "Deploy the todo app", Done: false},
+		{Text: "Take a break", Done: true},
+	}
+
 	r := gin.Default()
 	r.StaticFile("/", "./front/index.html")
+	r.StaticFile("/style.css", "./front/style.css")
 	r.StaticFile("/image.jpg", imagePath)
+
+	r.GET("/todos", func(c *gin.Context) {
+		mu.Lock()
+		defer mu.Unlock()
+		c.JSON(200, todos)
+	})
+
+	r.POST("/todos", func(c *gin.Context) {
+		var body newTodoRequest
+		if err := c.BindJSON(&body); err != nil {
+			c.JSON(400, gin.H{"error": "invalid request body"})
+			return
+		}
+
+		text := strings.TrimSpace(body.Text)
+		if text == "" {
+			c.JSON(400, gin.H{"error": "todo text must not be empty"})
+			return
+		}
+		if len(text) > maxTodoLength {
+			c.JSON(400, gin.H{"error": fmt.Sprintf("todo text must be %d characters or fewer", maxTodoLength)})
+			return
+		}
+
+		todo := Todo{Text: text, Done: false}
+		mu.Lock()
+		todos = append(todos, todo)
+		mu.Unlock()
+
+		c.JSON(201, todo)
+	})
 
 	fmt.Printf("Server started in port %s\n", port)
 	r.Run(":" + port)
