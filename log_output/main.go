0891ea45d21066d9ced0b395984ed5cae44573a0
SteveMCWin <stevp003@gmail.com>
2026-07-22 15:00:04 +0200

    1.7: added status endpoint in log output, as well as service and ingress manifest files

--- log_output/main.go
+++ log_output/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"crypto/rand"
 	"fmt"
+	"os"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func newUUID() string {
@@ -14,11 +17,33 @@ func newUUID() string {
 	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:16])
 }
 
+func timestamp() string {
+	return time.Now().UTC().Format("2006-01-02T15:04:05.000Z")
+}
+
 func main() {
 	id := newUUID()
 
-	for {
-		fmt.Printf("%s: %s\n", time.Now().UTC().Format("2006-01-02T15:04:05.000Z"), id)
-		time.Sleep(5 * time.Second)
+	go func() {
+		for {
+			fmt.Printf("%s: %s\n", timestamp(), id)
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5607"
 	}
+
+	r := gin.Default()
+	r.GET("/status", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"timestamp": timestamp(),
+			"string":    id,
+		})
+	})
+
+	fmt.Printf("Server started in port %s\n", port)
+	r.Run(":" + port)
 }
